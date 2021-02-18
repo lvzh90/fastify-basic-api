4895052f089b09177b2c811ef5ba92f4092129f4
lvzh90 <linda.zapata@mulesoft.com>
2021-02-18 00:05:07 -0500

    refactor(server): Add delivery layer to the application

---
+++ src/delivery/http/server.js
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+
+async function start() {
+  const fastify = Fastify();
+
+  fastify.get('/', () => ({ status: "Running" }));
+  fastify.get('/Santiago', () => {
+      return {
+          message: "Toy con la personita mas tesita del mundo y me hace muy feliz :)"
+        }
+    });
+
+  try {
+    await fastify.listen(8080);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+export default start;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-import Fastify from "fastify";
-
-async function start() {
-  const fastify = Fastify();
-
-  fastify.get('/', () => ({ status: "Running" }));
-  fastify.get('/Santiago', () => {
-      return {
-          message: "Toy con la personita mas tesita del mundo y me hace muy feliz :)"
-        }
-    });
-
-  try {
-    await fastify.listen(8080);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-}
+import start from './delivery/http/server.js';
 
 start()
   .then(() => console.log("Server running"))
